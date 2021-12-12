95ba3c6b3290b65140e6ca60b678f4c6b6e76eff
wahidikqbal <wahidikqbal@gmail.com>
2021-12-12 13:07:36 +0700

    menambahkan halaman create

--- pages/api/posts/create.js
+++ pages/api/posts/create.js
@@ -1,14 +1,22 @@
 import db from "../../../libs/db";
 
 export default async function handler (req, res) {
+
+	if(req.method !== 'POST') return res.status(405).end();
+
+	const { title, content } = req.body;
+
 	const create = await db('posts').insert({
-		title 	: 'test Post title',
-		content : "test post content",  
-	})
+		title,
+		content
+	});
+
+	const createdData = await db('posts').where('id', create).first();
 
 	res.status (200);
 	res.json ({
-		message: "Post created successfully"
+		message: "Post created successfully",
+		data: createdData
 	});
 }
 
