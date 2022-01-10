88c1b89bba1f04154ee21744a3fff8a0862f49bc
wahidikqbal <wahidikqbal@gmail.com>
2022-01-10 19:07:33 +0700

    menambahkan props pada halaman posts

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -6,20 +6,38 @@ export async function getServerSideProps(ctx) {
     //REDIRECT LANGSUNG JIKA ADA COOKIE//
     //console.log(await authPage(ctx))
     const allCookies = nookies(ctx)
-    // console.log(allCookies)
-    if(!allCookies.xToken) 
+    const token = allCookies.xToken
+    //console.log(token)
+
+    if(!token) 
         return await authPage(ctx)
 
 
+    const postReq = await fetch('http://localhost:3000/api/posts', {
+        headers: {
+            'Authorization': 'Bearer ' + token
+        }
+    });
+    
+    const posts = await postReq.json();
+    console.log(posts)
+
     return {
-      props: {}, // will be passed to the page component as props
+      props: {
+          posts : posts.data
+      }, // will be passed to the page component as props
     }
   }
 
-export default function postIndex () {
+export default function postIndex (props) {
     return (
         <div>
             <h1> Halaman Posts</h1>
+            { props.posts.map(post => {
+                return (
+                    <div key={post.id}> {post.title} , {post.content} </div>
+                )
+            }) }
         </div>
     )
 }
